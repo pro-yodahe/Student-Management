93e08292375b619228ac9c272bd09ef03c4f56e1
pro-yodahe <yodahejob@gmail.com>
2024-12-19 04:09:34 +0300

    Updated components, added CountChart and UserCard, and modified configurations

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 import Menu from "@/components/Menu";
+import Navbar from "@/components/Navbar";
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -9,7 +10,7 @@ export default function DashboardLayout({
     return (
       <div className="h-screen flex ">
         {/* left */}
-        <div className="w-[14%] md:[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
+        <div className="w-[14%] md:[8%] lg:w-[16%] xl:w-[14%] ">
         <Link href="/" className="flex items-center justify-center lg:justify-start gap-2">
         <Image src="/logo.png" alt="logo" width={32} height={32}/>
         <span className="hidden lg:block">Harmony</span>
@@ -17,8 +18,9 @@ export default function DashboardLayout({
         <Menu/>
         </div>
         {/* Right */}
-        <div className="w-[86%] md:[92%] lg:w-[84%] xl:w-[86%] bg-blue-200">
-          r
+        <div className="w-[86%] md:[92%] lg:w-[84%] xl:w-[86%] bg-[]">
+          <Navbar />
+          {children}
           </div>
       </div>
     );
